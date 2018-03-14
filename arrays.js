f37a4b45d28454cd2397d8448a0eff2c4a290389
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-14 21:47:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ var gailledCheeseIngredients=[
       'skittles']
       
       function addElementToBeginningOfArray(array,element){
-        array.unnshift(element);
+        array.unshift(element);
         return array;
       }
       function destructivelyAddElementToBeginningOfArray(array,element)
