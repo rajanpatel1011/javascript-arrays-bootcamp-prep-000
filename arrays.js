26d8dc7333b261555b2269594f57d2a8f8265075
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-14 21:32:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var ingredient1 ="bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+var gailledCheeseIngredients=[
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+  ]
+  var tomatoSauceIngredients = [
+    'tomato',
+    'garlic',
+    'olive oil',
+    'basil',
+    'oregano'
+    ]
+    var chocolateBars = [
+      'snicker',
+      'hundred grand',
+      'kitkat',
+      'skittles']
+      
+      function addElementToBeginningOfArray(array,element){
+        chocolateBars[element-1]=array;
+        return chocolateBars;
+      }
+      function destructivelyAddElementToBeginningOfArray(array,element)
+      {
+        var newArray = [];
+        newArray = chocolateBars;
+        newArray[element-1] = array;
+        return newArray;
+        
+      }
